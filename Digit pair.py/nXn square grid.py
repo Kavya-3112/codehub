6a51bc380b70651d26e02cdd340a4aa109a4a5d8
kavya K <125470243+Kavya-3112@users.noreply.github.com>
2023-10-06 22:26:46 +0530

    Update nXn square grid.py

--- Digit pair.py/nXn square grid.py
+++ Digit pair.py/nXn square grid.py
@@ -24,4 +24,40 @@ for _ in range(N):
 # Find the smallest score
 result = find_smallest_score(N, grid)
 print(result)
-    
+**************************************************************************
+    ques=race
+N = int(input())  # Number of participants
+T = int(input())  # Total time in seconds
+
+participants = []
+
+# Read participant data
+for _ in range(N):
+    data = list(map(int, input().split()))
+    participants.append(data)
+
+# Initialize a list to keep track of the number of times each participant leads
+leading_count = [0] * N
+
+# Calculate the leader at each time slice
+for t in range(T):
+    leader_distance = -1
+    leading_indices = []
+
+    for i in range(N):
+        distance = sum(participants[i][:t+1])  # Calculate the distance covered by the participant at time t
+        if distance >= leader_distance:
+            if distance > leader_distance:
+                leader_distance = distance
+                leading_indices = [i]
+            else:
+                leading_indices.append(i)
+
+    for i in leading_indices:
+        leading_count[i] += 1
+
+# Find the winner based on the leading count
+winner_index = leading_count.index(max(leading_count)) + 1
+
+# Print the winner's index
+print(winner_index)
