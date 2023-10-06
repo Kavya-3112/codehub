2d0a91ef372010532848e6f6ebe8da652d96ac03
kavya K <125470243+Kavya-3112@users.noreply.github.com>
2023-10-06 22:14:42 +0530

    Update swayamvaram.py

--- Digit pair.py/swayamvaram.py
+++ Digit pair.py/swayamvaram.py
@@ -8,3 +8,26 @@ for i in bride:
     else:
         break
 print(len(groom))
+question teacher school tcs
+def calculate_children(p, q, r, s):
+    total_children = 0
+    for length in range(p, q + 1):
+        for width in range(r, s + 1):
+            while length != width:
+                if length > width:
+                    length -= width
+                else:
+                    width -= length
+                total_children += 1
+            total_children += 1  # One more child for the remaining square
+    return total_children
+
+# Read input
+p = int(input())
+q = int(input())
+r = int(input())
+s = int(input())
+
+# Calculate and print the total number of children
+result = calculate_children(p, q, r, s)
+print(result)
