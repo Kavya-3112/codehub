bc7a50f41dd99f2932d4808347a06361614c2f16
kavya K <125470243+Kavya-3112@users.noreply.github.com>
2023-10-06 22:23:18 +0530

    Update swayamvaram.py

--- Digit pair.py/swayamvaram.py
+++ Digit pair.py/swayamvaram.py
@@ -8,7 +8,7 @@ for i in bride:
     else:
         break
 print(len(groom))
-question teacher school tcs
+question teacher school tcs********************************************************************************************
 def calculate_children(p, q, r, s):
     total_children = 0
     for length in range(p, q + 1):
@@ -31,3 +31,36 @@ s = int(input())
 # Calculate and print the total number of children
 result = calculate_children(p, q, r, s)
 print(result)
+atm=tcs****************************************************************************************
+N = int(input())
+withdrawal_amount = int(input())
+notes_100 = int(input())
+notes_200 = int(input())
+notes_500 = int(input())
+notes_1000 = int(input())
+
+# Calculate the maximum number of currency notes possible
+max_notes = 0
+
+while withdrawal_amount >= 100 and N > 0:
+    if withdrawal_amount >= 1000 and notes_1000 > 0:
+        withdrawal_amount -= 1000
+        notes_1000 -= 1
+        max_notes += 1
+    elif withdrawal_amount >= 500 and notes_500 > 0:
+        withdrawal_amount -= 500
+        notes_500 -= 1
+        max_notes += 1
+    elif withdrawal_amount >= 200 and notes_200 > 0:
+        withdrawal_amount -= 200
+        notes_200 -= 1
+        max_notes += 1
+    elif withdrawal_amount >= 100 and notes_100 > 0:
+        withdrawal_amount -= 100
+        notes_100 -= 1
+        max_notes += 1
+    else:
+        break
+
+# Print the maximum number of currency notes possible
+print(max_notes)
